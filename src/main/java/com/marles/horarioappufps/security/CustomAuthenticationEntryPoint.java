81c99b5a9748c4c8da6389713db860954017ad48
JorgeMarles <jamarlesf@gmail.com>
2025-07-31 14:06:35 -0500

    feat: Custom Error Handling for Authentication Exceptions

---
+++ src/main/java/com/marles/horarioappufps/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,38 @@
+package com.marles.horarioappufps.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("error", true);
+        body.put("message", "Not logged in.");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.getWriter().write(objectMapper.writeValueAsString(body));
+        response.getWriter().flush();
+
+    }
+
+
+}
+
