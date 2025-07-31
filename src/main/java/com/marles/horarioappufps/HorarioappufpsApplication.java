7f185e9568986614d532cd305ba0c2ab6532b977
JorgeMarles <jamarlesf@gmail.com>
2025-07-30 22:43:29 -0500

    first commit

---
+++ src/main/java/com/marles/horarioappufps/HorarioappufpsApplication.java
@@ -0,0 +1,13 @@
+package com.marles.horarioappufps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorarioappufpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorarioappufpsApplication.class, args);
+    }
+
+}
