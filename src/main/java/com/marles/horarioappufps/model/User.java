cbf812ad1a92b04234eb63d7e01ec2b02955d5c2
JorgeMarles <jamarlesf@gmail.com>
2025-07-31 14:06:55 -0500

    fix: first endpoint GET /users/all

--- src/main/java/com/marles/horarioappufps/model/User.java
+++ src/main/java/com/marles/horarioappufps/model/User.java
@@ -21,7 +21,7 @@ public class User {
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "users_roles",
             joinColumns =
-            @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            @JoinColumn(name = "user_id", referencedColumnName = "uid"),
             inverseJoinColumns =
             @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private Set<Role> roles;
