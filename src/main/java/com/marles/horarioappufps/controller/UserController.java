488c6a063d6601dd7fb7061fd77b7b6a7fef3324
JorgeMarles <jamarlesf@gmail.com>
2025-07-31 22:55:23 -0500

    feat: GET /users/{uid} and GET users/my

--- src/main/java/com/marles/horarioappufps/controller/UserController.java
+++ src/main/java/com/marles/horarioappufps/controller/UserController.java
@@ -2,11 +2,14 @@ package com.marles.horarioappufps.controller;
 
 import com.marles.horarioappufps.dto.response.UserDto;
 import com.marles.horarioappufps.model.User;
+import com.marles.horarioappufps.security.UserPrincipal;
 import com.marles.horarioappufps.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +28,18 @@ public class UserController {
         List<User> users = userService.getAllUsers();
         List<UserDto> response = UserDto.parseFrom(users);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{uid}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<UserDto> getUserByUid(@PathVariable String uid) {
+        return ResponseEntity.ok(UserDto.parseFrom(userService.getUserByUid(uid)));
+    }
+
+    @GetMapping("/my")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<UserDto> getMyUser(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+        String uid = userPrincipal.getUsername();
+        return ResponseEntity.ok(UserDto.parseFrom(userService.getUserByUid(uid)));
     }
 }
--- src/main/java/com/marles/horarioappufps/dto/response/UserDto.java
+++ src/main/java/com/marles/horarioappufps/dto/response/UserDto.java
@@ -1,22 +1,26 @@
 package com.marles.horarioappufps.dto.response;
 
+import com.marles.horarioappufps.model.Role;
 import com.marles.horarioappufps.model.User;
 import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class UserDto {
 
     private String uid;
     private String email;
+    private List<String> roles = new LinkedList<>();
 
     public static UserDto parseFrom(User user){
         UserDto userDto = new UserDto();
         userDto.setUid(user.getUid());
         userDto.setEmail(user.getEmail());
+        userDto.getRoles().addAll(user.getRoles().stream().map(Role::getName).toList());
         return userDto;
     }
 
