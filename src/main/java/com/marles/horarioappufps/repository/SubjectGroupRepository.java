b8f1254ff534b02cbae9f180c0fc910043426ef4
JorgeMarles <jamarlesf@gmail.com>
2025-08-03 14:36:44 -0500

    FEAT: Pensum saving first logic implemented (untested)

--- src/main/java/com/marles/horarioappufps/repository/SubjectGroupRepository.java
+++ src/main/java/com/marles/horarioappufps/repository/SubjectGroupRepository.java
@@ -3,5 +3,8 @@ package com.marles.horarioappufps.repository;
 import com.marles.horarioappufps.model.SubjectGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SubjectGroupRepository extends JpaRepository<SubjectGroup, Long> {
+    Optional<SubjectGroup> findByCode(String code);
 }
---
+++ src/main/java/com/marles/horarioappufps/service/PensumService.java
@@ -0,0 +1,191 @@
+package com.marles.horarioappufps.service;
+
+import com.marles.horarioappufps.dto.request.PensumCreationDto;
+import com.marles.horarioappufps.dto.request.SessionCreationDto;
+import com.marles.horarioappufps.dto.request.SubjectCreationDto;
+import com.marles.horarioappufps.dto.request.SubjectGroupCreationDto;
+import com.marles.horarioappufps.exception.PensumNotFoundException;
+import com.marles.horarioappufps.model.Pensum;
+import com.marles.horarioappufps.model.Session;
+import com.marles.horarioappufps.model.Subject;
+import com.marles.horarioappufps.model.SubjectGroup;
+import com.marles.horarioappufps.repository.PensumRepository;
+import com.marles.horarioappufps.repository.SessionRepository;
+import com.marles.horarioappufps.repository.SubjectGroupRepository;
+import com.marles.horarioappufps.repository.SubjectRepository;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@Transactional
+@Slf4j
+public class PensumService {
+
+    private final PensumRepository pensumRepository;
+    private final SubjectRepository subjectRepository;
+    private final SubjectGroupRepository subjectGroupRepository;
+    private final SessionRepository sessionRepository;
+
+    private boolean updateTeachers;
+
+    @Autowired
+    public PensumService(
+            PensumRepository pensumRepository,
+            SubjectRepository subjectRepository,
+            SubjectGroupRepository subjectGroupRepository,
+            SessionRepository sessionRepository
+    ) {
+        this.pensumRepository = pensumRepository;
+        this.subjectRepository = subjectRepository;
+        this.subjectGroupRepository = subjectGroupRepository;
+        this.sessionRepository = sessionRepository;
+    }
+
+    public Pensum savePensum(PensumCreationDto pensumCreationDto) {
+        Pensum pensum = new Pensum();
+        if (pensumCreationDto.getId() != null) {
+            pensum = pensumRepository.findById(pensumCreationDto.getId()).orElseThrow(() -> new PensumNotFoundException(pensumCreationDto.getId()));
+        }
+
+        updateFields(pensum, pensumCreationDto);
+
+        this.updateTeachers = pensumCreationDto.isUpdateTeachers();
+
+        pensum = pensumRepository.save(pensum);
+
+        processSubjects(pensum, pensumCreationDto.getSubjects());
+
+        return pensum;
+    }
+
+    private void processSubjects(Pensum pensum, List<SubjectCreationDto> subjectCreationDtos) {
+        pensum.getSubjects().clear();
+
+        Map<String, Subject> subjectMap = new HashMap<>();
+
+        for (SubjectCreationDto subjectCreationDto : subjectCreationDtos) {
+            Subject subject = createOrUpdateSubject(subjectCreationDto, pensum);
+            subjectMap.put(subject.getCode(), subject);
+            pensum.getSubjects().add(subject);
+        }
+
+        for (int i = 0; i < subjectCreationDtos.size(); i++) {
+            SubjectCreationDto subjectCreationDto = subjectCreationDtos.get(i);
+            Subject subject = pensum.getSubjects().get(i);
+            processGroups(subject, subjectCreationDto.getGroups());
+        }
+
+        for (int i = 0; i < subjectCreationDtos.size(); i++) {
+            SubjectCreationDto subjectCreationDto = subjectCreationDtos.get(i);
+            Subject subject = pensum.getSubjects().get(i);
+            processRequisite(subject, subjectCreationDto.getRequisites(), subjectMap);
+        }
+    }
+
+    private void processRequisite(Subject subject, List<String> requisites, Map<String, Subject> subjectMap) {
+        subject.getRequisites().clear();
+        for (String requisite : requisites) {
+            Subject subjectRequisite = subjectMap.get(requisite);
+            if (subjectRequisite != null) {
+                subject.getRequisites().add(subjectRequisite);
+            }
+        }
+        subjectRepository.save(subject);
+    }
+
+    private Subject createOrUpdateSubject(SubjectCreationDto subjectCreationDto, Pensum pensum) {
+        Subject subject = new Subject();
+        Optional<Subject> subjectOpt = subjectRepository.findByCode(subjectCreationDto.getCode());
+        if (subjectOpt.isPresent()) {
+            subject = subjectOpt.get();
+        }
+
+        updateFields(subject, subjectCreationDto);
+        subject.setPensum(pensum);
+
+        return subjectRepository.save(subject);
+    }
+
+    private void processGroups(Subject subject, List<SubjectGroupCreationDto> subjectGroupCreationDtos) {
+        subject.getGroups().clear();
+
+        for (SubjectGroupCreationDto subjectGroupCreationDto : subjectGroupCreationDtos) {
+            SubjectGroup subjectGroup = createOrUpdateSubjectGroup(subjectGroupCreationDto, subject);
+            subject.getGroups().add(subjectGroup);
+
+            processSessions(subjectGroup, subjectGroupCreationDto.getSessions());
+        }
+    }
+
+    private void processSessions(SubjectGroup subjectGroup, List<SessionCreationDto> sessionCreationDtos) {
+        subjectGroup.getSessions().clear();
+        for (SessionCreationDto sessionCreationDto : sessionCreationDtos) {
+            Session session = createSession(sessionCreationDto, subjectGroup);
+            subjectGroup.getSessions().add(session);
+        }
+    }
+
+    public Session createSession(SessionCreationDto sessionCreationDto, SubjectGroup subjectGroup) {
+        Session session = new Session();
+
+        updateFields(session, sessionCreationDto);
+        session.setGroup(subjectGroup);
+
+        return sessionRepository.save(session);
+    }
+
+    public SubjectGroup createOrUpdateSubjectGroup(SubjectGroupCreationDto subjectGroupCreationDto, Subject subject) {
+        SubjectGroup subjectGroup = new SubjectGroup();
+        Optional<SubjectGroup> subjectGroupOpt = subjectGroupRepository.findByCode(subjectGroupCreationDto.getCode());
+        if (subjectGroupOpt.isPresent()) {
+            subjectGroup = subjectGroupOpt.get();
+        }
+
+        updateFields(subjectGroup, subjectGroupCreationDto);
+        subjectGroup.setSubject(subject);
+
+        return subjectGroupRepository.save(subjectGroup);
+    }
+
+    private void updateFields(Pensum pensum, PensumCreationDto pensumCreationDto) {
+        pensum.setName(pensumCreationDto.getName());
+        pensum.setSemesters(pensumCreationDto.getSemesters());
+    }
+
+    private void updateFields(Subject subject, SubjectCreationDto subjectCreationDto) {
+        subject.setName(subjectCreationDto.getName());
+        subject.setCode(subjectCreationDto.getCode());
+        subject.setCredits(subjectCreationDto.getCredits());
+        subject.setHours(subjectCreationDto.getHours());
+        subject.setRequiredCredits(subjectCreationDto.getRequiredCredits());
+        subject.setSemester(subjectCreationDto.getSemester());
+        subject.setType(subjectCreationDto.getType());
+    }
+
+    private void updateFields(SubjectGroup subjectGroup, SubjectGroupCreationDto subjectGroupCreationDto) {
+        subjectGroup.setCode(subjectGroupCreationDto.getCode());
+        subjectGroup.setProgram(subjectGroupCreationDto.getProgram());
+        subjectGroup.setAvailableCapacity(subjectGroupCreationDto.getAvailableCapacity());
+        subjectGroup.setMaxCapacity(subjectGroupCreationDto.getMaxCapacity());
+        if (!"-".equals(subjectGroupCreationDto.getTeacher())) {
+            subjectGroup.setTeacher(subjectGroupCreationDto.getTeacher());
+            subjectGroup.setCurrentTeacher(true);
+        } else if (this.updateTeachers) {
+            subjectGroup.setCurrentTeacher(false);
+        }
+    }
+
+    private void updateFields(Session session, SessionCreationDto sessionCreationDto) {
+        session.setClassroom(sessionCreationDto.getClassroom());
+        session.setDay(sessionCreationDto.getDay());
+        session.setHour(sessionCreationDto.getHour());
+    }
+
+}
