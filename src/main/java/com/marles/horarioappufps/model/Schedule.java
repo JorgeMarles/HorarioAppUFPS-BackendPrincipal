23978b6b5dd1c835d7cd693a2c61b0b850bbc113
JorgeMarles <jamarlesf@gmail.com>
2025-08-05 17:37:49 -0500

    chore: Update and create models

---
+++ src/main/java/com/marles/horarioappufps/model/Schedule.java
@@ -0,0 +1,34 @@
+package com.marles.horarioappufps.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pensum_id", nullable = false)
+    private Pensum pensum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_uid", nullable = false)
+    private User user;
+
+    @ElementCollection
+    @CollectionTable(name = "schedule_enrollments",
+            joinColumns = @JoinColumn(name = "schedule_id"))
+    @Column(name = "group_code")
+    private List<String> codes = new LinkedList<>();
+
+}
