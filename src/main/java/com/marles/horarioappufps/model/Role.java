d80082e859ada126a321411959785113b35093e0
JorgeMarles <jamarlesf@gmail.com>
2025-10-01 22:37:48 -0500

    fix: Role GenerationType

--- src/main/java/com/marles/horarioappufps/model/Role.java
+++ src/main/java/com/marles/horarioappufps/model/Role.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
